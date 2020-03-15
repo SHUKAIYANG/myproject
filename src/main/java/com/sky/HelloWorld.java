b554050f707dedfcaaf7f11934999542b37d4439
SK Yang <rock5215@yahoo.com.tw>
2020-03-15 22:25:41 +0800

    Java and Kotlin variable practice

--- src/main/java/com/sky/HelloWorld.java
+++ src/main/java/com/sky/HelloWorld.java
@@ -6,6 +6,22 @@ public class HelloWorld {
         Person p = new Person();
         p.hello();
 
-        
+        int age = 19;
+        short age2 = 19;
+        long age3 = 19;
+        Long age4;
+        Integer i = 120;
+        char c = 'A';
+        Character c2 = 'A';
+        byte b = 120;
+        float f= 1.75f;
+        Float f2 = 1.75f;
+        double d = 1.75;
+        Double d2 = 1.75;
+        boolean bo1 = true;
+        boolean bo2 = false;
+        Boolean bo3;
+        String name = "Tom Curse";
+
     }
 }
--- src/main/kotlin/com/sky/Hello.kt
+++ src/main/kotlin/com/sky/Hello.kt
@@ -7,6 +7,30 @@ fun main() {
 
     val h = Human()
     h.hello()
+
+    val age = 19
+    val age2 : Int = 19
+
+    var age3 = 19
+    var age4 : Int = 19
+
+    var weight = 66.5f
+    var weight2 : Float = 66.5f
+
+    var name : String
+    name = "Tom Curse"
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
 class Human {
